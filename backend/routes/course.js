82d493c85698ef254d76923eba3f9ff7983362bb
Binoosh <wbinoosh@gmail.com>
2024-03-20 16:36:28 +0530

    Functionality to update the faculty to which a course belongs, and authorization implemented

--- backend/routes/course.js
+++ backend/routes/course.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { addCourse, updateCourse, getAllCourses, deleteCourse } = require('../controllers/courseController');
+const { addCourse, updateCourse, getAllCourses, deleteCourse, updateFaculty } = require('../controllers/courseController');
 const { authUser } = require('../utils/authUser');
 
 router.post('/add', authUser, addCourse);
 router.put('/update/:crscode', updateCourse);
+router.put('/update/faculty/:crscode', authUser,updateFaculty);
 router.get('/', getAllCourses);
 router.delete('/delete/:crscode', deleteCourse);
 
