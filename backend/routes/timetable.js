3f7e7325bd7fc705afe85be1ff7b8c2db9f5aaf2
Binoosh <wbinoosh@gmail.com>
2024-03-21 20:06:30 +0530

    Delete class feature implemented, and some errors were fixed

--- backend/routes/timetable.js
+++ backend/routes/timetable.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const {addClass, updateClass} = require('../controllers/timetableController');
+const {addClass, updateClass, deleteClass} = require('../controllers/timetableController');
 const {authUser} = require('../utils/authUser');
+
 
 router.post('/add', authUser, addClass);
 router.put('/update/:classID', authUser, updateClass);
+router.delete('/delete/:classID', authUser, deleteClass);
 
 module.exports = router;
